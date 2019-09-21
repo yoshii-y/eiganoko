d687a111785bb57bdb19ebbb9e517f2a0a674de3
yoshii-y <science_yy@yahoo.co.jp>
2019-09-21 17:37:45 +0900

    delete

--- db/migrate/20190824071136_create_relationship.rb
+++
@@ -1,7 +0,0 @@
-class CreateRelatonship < ActiveRecord::Migration[5.1]
-    def change
-      create_table :relationships do |t|
-        t.timestamps
-      end
-    end
-  end
--- db/migrate/20190825142849_relationship.rb
+++
@@ -1,5 +0,0 @@
-class Relationship < ActiveRecord::Migration[5.1]
-  def change
-    drop_table :relationships
-  end
-end
